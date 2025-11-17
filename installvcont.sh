9cafa51facca2ba75ef3c56e89fa8c80d1c953c3
severin <128835042+seve05@users.noreply.github.com>
2025-11-17 23:19:13 +0100

    change updog

--- installvcont.sh
+++ installvcont.sh
@@ -5,7 +5,7 @@ read -p "Continue? (Y/N): " confirm && [[ $confirm == [yYj] || $confirm == [yYjJ
 #exit1 if error such that user can get the ip right
 echo $IP > nfsipaddr.txt
 echo "You can change the IP in ~bin/nfsipaddr.txt"
-cat > updog << 'EOF'
+cat > vcont << 'EOF'
 #!/bin/bash
 IPadrpath="./ipaddr.txt"
 IPadr=$(cat "$IPadrpath")
@@ -14,8 +14,8 @@ dest="$HOME/networkfolder"
 cp -f "$1" "$dest"
 EOF
 mkdir -p "$HOME/networkfolder"
-sudo chmod +x updog
-sudo mv updog ~bin/
+sudo chmod +x vcont
+sudo mv vcont ~bin/
 sudo mv ipaddr.txt ~bin/
 #now we need to install NFS Client if not present, configure NFS IP on client
 sudo apt install nfs-common
